5d737a921311396cb9a47cf4dee65d3e69d45b29
greidinger-reis <jp.greidinger@hotmail.com>
2023-05-27 16:27:01 -0300

    favorites page article fetch fix

--- src/app/profile/[username]/favorites/loading.tsx
+++ src/app/profile/[username]/favorites/loading.tsx
@@ -1,3 +1,3 @@
 export default function Loading() {
-    return <div>Loading articles...</div>
+    return <div className="p-4">Loading articles...</div>
 }
--- src/app/profile/[username]/favorites/page.tsx
+++ src/app/profile/[username]/favorites/page.tsx
@@ -1,6 +1,5 @@
 import { getServerSession } from "next-auth"
-import { cookies } from "next/headers"
-import { USER_TOKEN } from "~/config/constants"
+import { ArticleList } from "~/components/articles/article-list"
 import { articlesService } from "~/modules/articles/articles.service"
 import { authOptions } from "~/modules/auth/auth.options"
 
@@ -10,16 +9,12 @@ export default async function FavoritesPage({
     params: { username: string }
 }) {
     const session = await getServerSession(authOptions)
-    const articles = await articlesService.getArticles({
-        currentUserId: session?.user?.id ?? null,
-        feedType: "global",
-        params: {
-            authorName: null,
-            tag: null,
-            limit: 6,
-            offset: 0,
-            favoritedBy: params.username,
-        },
-    })
-    return <pre>{JSON.stringify(articles, null, 4)}</pre>
+    const articles = await articlesService.getAllFavoritedByUser(
+        params.username,
+        session?.user?.id,
+        10,
+        0,
+    )
+
+    return <ArticleList articles={articles} />
 }
